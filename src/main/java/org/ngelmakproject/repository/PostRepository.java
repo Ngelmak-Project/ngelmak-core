586d24d3594c2664cdc5f539654bf8d1ec0fa03c
Youssouph Faye <youssouph.faye@gmail.com>
2026-03-06 20:22:35 +0100

    Fast update

--- src/main/java/org/ngelmakproject/repository/PostRepository.java
+++ src/main/java/org/ngelmakproject/repository/PostRepository.java
@@ -79,14 +79,14 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	@Query(value = """
 			SELECT DISTINCT p.*,
 			       (
-			           (EXTRACT(EPOCH FROM p.created_at) * 0.8) +
+			           (EXTRACT(EPOCH FROM p.at) * 0.8) +
 			           (LEAST(p.comment_count, 20) * 0.2) +
 			           ((hashtext(CONCAT(:sessionKey, '-', p.id)) % 1000) / 1000.0 * 300)
 			       ) AS score
 			FROM nk_post p
 			LEFT JOIN nk_channel c ON c.id = p.channel_id
-			LEFT JOIN post r ON r.post_reply_id = p.id
-			WHERE p.created_at >= :windowStart
+			LEFT JOIN nk_post r ON r.post_reply_id = p.id
+			WHERE p.at >= :windowStart
 			ORDER BY score DESC
 			LIMIT :limit OFFSET :offset
 			""", nativeQuery = true)
