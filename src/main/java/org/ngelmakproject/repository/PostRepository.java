c3bfa47e52138beb306bc295cc2e7211a4a314be
Youssouph Faye <youssouph.faye@gmail.com>
2026-03-06 20:27:56 +0100

    Feed query updated

--- src/main/java/org/ngelmakproject/repository/PostRepository.java
+++ src/main/java/org/ngelmakproject/repository/PostRepository.java
@@ -78,18 +78,20 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
 	@Query(value = """
 			SELECT DISTINCT p.*,
-			       (
-			           (EXTRACT(EPOCH FROM p.at) * 0.8) +
-			           (LEAST(p.comment_count, 20) * 0.2) +
-			           ((hashtext(CONCAT(:sessionKey, '-', p.id)) % 1000) / 1000.0 * 300)
-			       ) AS score
+				(
+						(EXTRACT(EPOCH FROM p.at) * 0.8) +
+						(LEAST(p.comment_count, 20) * 0.2) +
+						((hashtext(CONCAT(:sessionKey, '-', p.id)) % 1000) / 1000.0 * 300)
+				) AS score
 			FROM nk_post p
 			LEFT JOIN nk_channel c ON c.id = p.channel_id
 			LEFT JOIN nk_post r ON r.post_reply_id = p.id
+			LEFT JOIN nk_post_file pf ON pf.post_id = p.id
+			LEFT JOIN nk_file f ON f.id = pf.file_id
 			WHERE p.at >= :windowStart
 			ORDER BY score DESC
-			LIMIT :limit OFFSET :offset
-			""", nativeQuery = true)
+			LIMIT :limit OFFSET :offset;
+						""", nativeQuery = true)
 	List<Post> fetchFeedWithRelations(
 			@Param("sessionKey") String sessionKey,
 			@Param("windowStart") Instant windowStart,
