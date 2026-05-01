d9703bb3657746b010578d5c36c5ecd273321110
Youssouph Faye <youssouph.faye@gmail.com>
2026-05-01 18:43:44 +0200

    Fast update

--- src/main/java/org/ngelmakproject/service/operation/Operation.java
+++ src/main/java/org/ngelmakproject/service/operation/Operation.java
@@ -8,6 +8,8 @@ import java.util.UUID;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * Represents an operation to be performed on an entity.
@@ -17,7 +19,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class Operation<T> implements Serializable {
     private static final long serialVersionUID = 1L;
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final ObjectMapper MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
     public enum OperationType {
         CREATE, UPDATE, DELETE, DEFAULT
